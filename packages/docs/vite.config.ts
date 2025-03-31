8076f45e28e0db7c78a972c84abd4de178afd42d
Nicolas Reis <nicolas.e.silva@ufv.br>
2025-03-31 07:59:32 -0300

    Adicionando StoryBook.

---
+++ packages/docs/vite.config.ts
@@ -0,0 +1,14 @@
+//// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      path: 'path-browserify',
+    },
+  },
+})
